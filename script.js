1465628aaa62071f2b4c5a797887b2d55390a667
ABISHEK S <abishek28somu@gmail.com>
2024-12-26 19:21:51 +0530

    First Commit
    First Date:26.12.2024

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('manage-services').addEventListener('click', function() {
+    window.location.href = 'manage-services.html';
+});
+
+document.getElementById('manage-staff').addEventListener('click', function() {
+    window.location.href = 'manage-staff.html';
+});
